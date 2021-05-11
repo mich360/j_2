57c53b1706fc7463418a49a5fb7224d3d7e4bf92
mich360 <65708357+mich360@users.noreply.github.com>
2021-05-11 15:29:14 +0900

    test
    export function

---
+++ index.js
@@ -0,0 +1,5 @@
+import { name ,name2,log} from "./user.js";
+
+log(name);
+
+document.body.textContent = name + name2;
---
+++ user.js
@@ -0,0 +1,10 @@
+export const name = "みっちゃん";
+export const name2 = "まっちゃんちゃん";
+
+export function log(value) {
+    console.log(value);
+};
+
+//export { log };
+
+//export { name ,name2};
